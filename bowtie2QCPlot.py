a5eaa71c310464805bcd973d54a5353c8439ee03
whuang022ai <whuang022.ai@gmail.com>
2025-05-22 20:58:42 -0700

    add plot_bars_of_bowtie2_log

--- bowtie2QCPlot.py
+++ bowtie2QCPlot.py
@@ -1,4 +1,3 @@
-
 def plot_bar(ax, labels, sizes, title, **kwargs):
     '''
     plot bar
@@ -15,7 +14,11 @@ def plot_bar(ax, labels, sizes, title, **kwargs):
 
     for idx, value in enumerate(sizes):
         percentage = (value / total) * 100 if total else 0
-        ax.text(value + offset, idx, f'{percentage:.1f}%', va='center', fontsize=5)
+        ax.text(value + offset,
+                idx,
+                f'{percentage:.1f}%',
+                va='center',
+                fontsize=5)
 
     ax.set_xlim(1, max(sizes) * 20 if max(sizes) > 0 else 10)
     ax.set_xscale('log')
@@ -23,22 +26,24 @@ def plot_bar(ax, labels, sizes, title, **kwargs):
 
 
 def plot_bars_of_bowtie2_log(data, ax):
-
+    '''
+    plot bars of a bowtie2 pe log
+    '''
     labels_sets = [['0', '1', '>1'], ['1', '0'], ['0', '1', '>1']]
 
     sizes_sets = [[
         data['concordant_0']['count'], data['concordant_1']['count'],
         data['concordant_more']['count']
     ],
-    [
-        data['discordant_1']['count'],
-        data['concordant_0']['count'] +
-        data['discordant_1']['count']
-    ],
-    [
-        data['mate_0']['count'], data['mate_1']['count'],
-        data['mate_more']['count']
-    ]]
+                  [
+                      data['discordant_1']['count'],
+                      data['concordant_0']['count'] +
+                      data['discordant_1']['count']
+                  ],
+                  [
+                      data['mate_0']['count'], data['mate_1']['count'],
+                      data['mate_more']['count']
+                  ]]
 
     titles = [
         f'{data["log_file"]}\nConcordant N',
@@ -50,7 +55,6 @@ def plot_bars_of_bowtie2_log(data, ax):
     offsets = [500000, 500000, 100000]
 
     for i in range(3):
-        print( sizes_sets[i])
         plot_bar(ax[i],
                  labels_sets[i],
                  sizes_sets[i],
